b1b58c483bde6e87c6ded6c330fc5a0e357e3f46
Tugay Türk <i.tugayturk@gmail.com>
2020-12-31 00:18:24 +0000

    ödev çözümü

--- odev.js
+++ odev.js
@@ -34,3 +34,60 @@ document.querySelector("#reset").addEventListener("click", () => {
 /* START CODING HERE */
 
 
+const firstButton = document.querySelector("#populationBigger")
+firstButton.addEventListener("click", () => {
+    let bt500 = data.filter((city) => city.population > 500000)
+    createTableElements(bt500, "allcities");
+    createTableElements([], "singlecity");
+});
+//second green button
+const secondButton = document.querySelector("#landAreaLess")
+secondButton.addEventListener("click", () => {
+    let lt1000 = data.filter((city) => city.landArea < 1000);
+    createTableElements(lt1000, "allcities");
+    createTableElements([], "singlecity")
+})
+//blue buttons
+
+//isPopulationLess
+const thirdButton = document.querySelector("#isPopulationLess")
+thirdButton.addEventListener("click", () => {
+    let populationLess = data.some((less) => less.population < 100000)
+    if (populationLess) {
+        alert("YES");
+    } else {
+        alert("NO");
+    }
+}
+)
+
+
+//isLandBigger
+const fourthButton = document.querySelector("#isLandBigger")
+fourthButton.addEventListener("click", () => {
+    let landBigger = data.every((city) => city.landArea > 100);
+    if (landBigger) {
+        alert("YES");
+
+    } else {
+        alert("No");
+    }
+});
+
+//Select box
+
+const cityName = data.map(cityName => cityName.name);
+const citySelect = document.querySelector(".custom-select");
+cityName.forEach((element)=>{
+    
+    const cityCreate = document.createElement("option");
+    cityCreate.setAttribute("value",element);
+    cityCreate.textContent= element;
+    citySelect.appendChild(cityCreate);
+})
+
+citySelect.addEventListener("change", (e) => {
+    const selectCities = data.filter(cities => e.target.value === cities.name);
+
+    createTableElements(selectCities, "singlecity")
+})
